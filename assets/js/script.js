4e8302821f91f63cd10227ac286823ed3abace69
Symphony <55611595+lnance6@users.noreply.github.com>
2020-08-30 23:33:56 -0700

    quiz and timer working

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,14 +71,16 @@ var myQuestions = [
 
 var quizContainer = document.getElementById("quiz");
 var startButton = document.getElementById("start");
-var resultsContainer = document.getElementById("results");
+var resultsContainer = document.querySelector("#resultsText");
 var timerEl = document.querySelector(".Timer-Clock");
-var seconds = 75
+var seconds = 75;
 var headDiv = document.getElementById("head");
 var pDiv = document.getElementById("para");
+var newHeadDiv = document.getElementById("qHead");
+var newPDiv = document.getElementById("choices");
 var currentQuestion = 0;
 var wrapper = document.querySelector(".wrapper");
-var score = 0
+var score = 0;
 
 // Function for start time. Within this a setInterval () that will repeatly call a function at 1000 milliseconds.  The clearInterval () is called to stop the timer when seconds equal zero.
 
@@ -94,7 +96,9 @@ function startTime() {
     )
 }
 
-// Event listener called the startTime () to begin the timer.  Next the hideStartArea () is called to hide the <h1>, <p>, and <button> elements. Then the displayQuestion () is called to display the first question.
+// Event listener called the startTime () to begin the timer.  
+// Next the hideStartArea () is called to hide the <h1>, <p>, and <button> elements. 
+// Then the displayQuestion () is called to display the first question.
 
 startButton.addEventListener("click", function () {
     console.log("anything")
@@ -112,25 +116,44 @@ function hideStartArea() {
     startButton.style.display = "none"
 }
 
-// When the user clicks on one of the choices, a series of actions happens. The srcElement event references which choice was clicked on, and given the variable clickID. An answer variable was created so I can see in the console log which choice was selected.  correctID variable was created to compare to the correctAnswer. If they match, then the score increments.
+function hideMainArea() {
+
+    newHeadDiv.style.display = "none";
+    newPDiv.style.display = "none";
+    //startButton.style.display = "none"
+}
+
+// When the user clicks on one of the choices, a series of actions happens. 
+// The srcElement event references which choice was clicked on, and given the variable clickID. 
+// An answer variable was created so I can see in the console log which choice was selected.  
+// correctID variable was created to compare to the correctAnswer. 
+// If they match, then the score increments.
 
 function clickedButton(clickevent) {
 
     if (currentQuestion < myQuestions.length) {
-        var clickId = clickevent.srcElement.getAttribute('which');
+        var clickId = clickevent.srcElement.getAttribute('userClicked');
         var answered = myQuestions[currentQuestion].choices[clickId];
         console.log("User clicked " + clickId + " which is " + answered);
         var correctId = myQuestions[currentQuestion].correctAnswer;
         var correctAnswer = myQuestions[currentQuestion].choices[correctId];
         console.log("Correct answer is: " + correctId + " which is " + correctAnswer);
-
-        if (clickId == correctId) {
-            alert("Correct!");
+       
+       
+       if (clickId == correctId) {
+            
+            alert("Correct");
+              
+            //
+        
             score++
         } else {
-            alert("Wrong")
+            
+            seconds=seconds-10;
+            alert("Wrong");
+            //resultsContainer.textContent = "Wrong!";
         }
-
+        
         currentQuestion++
     }
 
@@ -138,6 +161,7 @@ function clickedButton(clickevent) {
     if (currentQuestion < myQuestions.length) {
         displayQuestion();
     } else {
+        displayScore();
         alert("The quiz is complete. Your score is " + score + " out of 10");
     }
 }
@@ -146,32 +170,55 @@ function clickedButton(clickevent) {
 
 function displayQuestion() {
 
-
     var question = document.createElement("h1");
+    question.setAttribute('id', 'qHead');
     question.textContent = myQuestions[currentQuestion].question;
     wrapper.innerHTML = ""; // clear everything in wrapper
     wrapper.appendChild(question);
 
     var choicesEl = document.createElement("div");
-    choicesEl.setAttribute('class', 'choices');
-
+    choicesEl.setAttribute('id', 'choices');
 
     for (var i = 0; i < myQuestions[currentQuestion].choices.length; i++) {
         console.log(myQuestions[currentQuestion].choices[i])
         var buttonEl = document.createElement("button");
         buttonEl.setAttribute('class', 'choice');
-        buttonEl.setAttribute('which', i);
+        buttonEl.setAttribute('userClicked', i);
         buttonEl.onclick = clickedButton;
         buttonEl.textContent = myQuestions[currentQuestion].choices[i];
         choicesEl.appendChild(buttonEl);
     }
 
     wrapper.appendChild(choicesEl);
+    
 }
 
 wrapper.addEventListener('click', function (event) {
     var target = event.currentTarget;
     if (target.classList.contains('choice')) {
+    }
+});
+
+
+var displayScore=function() {
+
+    var question = document.createElement("h1");
+    endQuiz.setAttribute('id', 'qHead');
+    endQuiz.textContent = "All done!";
+    wrapper.innerHTML = ""; // clear everything in wrapper
+    wrapper.appendChild(endQuiz);
+
+    var displayScoreEl = document.createElement("p");
+    displayScoreEl.setAttribute('id', 'finPara');
+    displayScoreEl.setAttribute('textContent', 'Your final score is ' + score);
+    
+    wrapper.appendChild(displayScoreEl);
+    
+}
+
+    wrapper.addEventListener('click', function (event) {
+    var target = event.currentTarget;
+    if (target.classList.contains('choice')) {
 
     }
-})
+});
